fa409e78b3d4d8904d56792255994c863eacd378
April Urnau <caiournau@gmail.com>
2022-12-15 21:44:12 -0300

    Começado o desenvolvimento do projeto

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+/// 
+/// Projeto final de Desenvolvimento de Software - "Feudo"
+/// Grupo Amarelo Girassol
+/// Semestre 2, 2022
+/// Universidade de Brasília, Campus do Gama
+///
+/**
+ * A função principal do projeto.
+ * @returns {void}
+ */
+function run() {
+    console.log("Amarelo girassol! :innocent:");
+}
+// Execute o código.
+run();
+export {};
+//# sourceMappingURL=index.js.map
