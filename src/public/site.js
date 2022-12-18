6fdef33353e7a10a4cd30651ca5e6be4447a7646
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2022-12-18 17:15:06 -0300

    o chat ta funcionando :)

--- src/public/site.js
+++ src/public/site.js
@@ -1,48 +1,51 @@
 const inputTexto = document.getElementById('enviarMensagem');
-const getLocalStorage = () => JSON.parse(localStorage.getItem('usuario')) ?? [];
+const btnSair  = document.getElementById('btnSair');
+const getLocalStorage = () =>JSON.parse(localStorage.getItem('usuario')) ?? [];
 const socket = io();
-const {usuarionome, meuid} = Qs.parse(location.search, {ignoreQueryPrefix: true});
-socket.emit("entrarSala", {usuarionome, meuid});
+const { usuarionome, meuid } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
-//------------- COLOCANDO A MSG NO CHAT -------------// 
-inputTexto.addEventListener("keyup", function(e){
+socket.emit('entrarSala', { usuarionome, meuid});
+
+inputTexto.addEventListener('keyup', function(e){
     var key = e.key === 'Enter';
-    if(key && this.value){
-        adicionarNovaMensagem(this.value);
+    if(key && this.value) {
+        socket.emit('mensagemChat', this.value);
         this.value = '';
-
     }
 });
 
-
-//---------------- COLOCANDO NO HTML ----------------// 
-function criarElementoHtml(nomeElemento, classeElemento){
-    var elemento = document.createElement(nomeElemento);
-    for(var classe of classeElemento){
-        elemento.classList.add(classe);
+btnSair.addEventListener('click', function() {
+    const sairSala = confirm('Certeza que deseja sair da sala?');
+    if (sairSala) {
+        socket.emit('sairSala');
+        window.location.href='index.html';
     }
-    return elemento;
-}
+    
+});
 
+function adicionarNovaMensagem(mensagem) {
+    const usuarioStorage = getLocalStorage();
+    let minhaMensagem = false;
+    if(mensagem.meuid) {
+        minhaMensagem = mensagem.meuid === usuarioStorage.meuId;
+    }
 
-//----------------- SCROLL DO CHAT -----------------//
-function realizarScrollChat(){
-    var elemento = document.getElementById('chat');
-    elemento.scrollTop = elemento.scrollHeight; 
-}
-
-
-//-------------- SEPARANDO A MENSAGEM --------------//
-function adicionarNovaMensagem(mensagem){
+    var divMensagem = '';
+    var divDetalhes = '';
     var quadroMensagens = document.getElementById('quadro-mensagens');
     var li = criarElementoHtml('li', ['clearfix']);
     var span = criarElementoHtml('span', ['message-data-time']);
-    var divMensagem = criarElementoHtml('div', ['message', 'my-message']);
-    var divDetalhes = criarElementoHtml('div', ['message-data']);
 
-    span.innerHTML = 'Nome Teste, 12 Dec 2022 - 21:15';
+    if(minhaMensagem) {
+        divMensagem = criarElementoHtml('div', ['message', 'other-message', 'float-right' ]);
+        divDetalhes = criarElementoHtml('div', ['message-data', 'text-right']);
+    } else {
+        divMensagem = criarElementoHtml('div', ['message', 'my-message']);
+        divDetalhes = criarElementoHtml('div', ['message-data']);
+    }
+
+    span.innerHTML = (minhaMensagem ? "eu" : mensagem.usuarioNome) + ', ' + mensagem.horario;
     divMensagem.innerHTML = mensagem.mensagem;
-
     divDetalhes.appendChild(span);
     li.appendChild(divDetalhes);
     li.appendChild(divMensagem);
@@ -50,6 +53,44 @@ function adicionarNovaMensagem(mensagem){
     realizarScrollChat();
 }
 
+function criarElementoHtml(nomeElemento, classeElemento, atributosElemento) {
+    var elemento = document.createElement(nomeElemento);
+    for (var classe of classeElemento) {
+        elemento.classList.add(classe);
+    }
+    return elemento;
+}
+
+function realizarScrollChat() {
+    var elem = document.getElementById('chat');
+    elem.scrollTop = elem.scrollHeight;
+}
+
+socket.on('salaUsuarios', ({sala, usuarios}) => {
+    document.getElementById("salaId").innerHTML = sala;
+    document.getElementById("listaUsuarios").innerHTML = '';
+    for (var usuario of usuarios) {
+        criarListaUsuarios(usuario.nome);
+    }
+});
+
 socket.on('novaMensagem', (mensagem) => {
     adicionarNovaMensagem(mensagem);
 });
+
+function criarListaUsuarios(usuarioNome) {
+    var listaUsuarios = document.getElementById("listaUsuarios");
+    var liUsuario = criarElementoHtml("li", ["clearfix"]);
+    var divDescricaoUsuario = criarElementoHtml('div', ["about"]);
+    var divNomeUsuario = criarElementoHtml('div', ["name"]);
+    var divStatusUsuario = criarElementoHtml('div', ["status"]);
+    var iconeStatus = criarElementoHtml("i" , ["fa", "fa-circle", "online"]);
+
+    iconeStatus.innerHTML = "online";
+    divNomeUsuario.innerHTML = usuarioNome;
+    divStatusUsuario.appendChild(iconeStatus);
+    divDescricaoUsuario.appendChild(divNomeUsuario);
+    divDescricaoUsuario.appendChild(divStatusUsuario);
+    liUsuario.appendChild(divDescricaoUsuario);
+    listaUsuarios.appendChild(liUsuario);
+}
