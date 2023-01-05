98cc60fb812d42f6b3b64f19c47806f4fa1d5b06
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-01-04 23:30:28 -0300

    terminando a interaçao do chat

--- src/public/interativide.js
+++ src/public/interativide.js
@@ -8,3 +8,18 @@ function menuShow() {
         ul.classList.add('open');
     }
 }
+
+var btn = document.getElementById("mostrar");
+var container = document.querySelector(".card");
+
+btn.addEventListener("click", function() {
+    
+  if(container.style.display === "block") {
+        container.style.display = "none";
+    } else {
+      container.style.display = "block";
+  }
+    
+});
+
+
