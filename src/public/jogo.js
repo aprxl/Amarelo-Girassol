006198934b9526575ac85cce015f2b3480202269
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-01-18 18:31:27 -0300

    Update jogo.js

--- src/public/jogo.js
+++ src/public/jogo.js
@@ -4,13 +4,15 @@ const carta1 = [
 const primeiracarta = carta1[(Math.floor(Math.random() * (carta1.length)))];
 const segundacarta = carta1[(Math.floor(Math.random() * (carta1.length)))];
 
+console.log(primeiracarta, segundacarta)
 
-function iniciarjogo(primeiracarta, segundacarta){
+
+/* function embaralhar(primeiracarta, segundacarta){
    
     document.getElementById("primeircarta").innerHTML = "primeira" + primeiracarta;
     document.getElementById("segundacarta").innerHTML = "segunda" + segundacarta;
 
-}
+} */
 
 
 
